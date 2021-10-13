f1259a80f94246c0b0a6719c87c1f011cd423c9e
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-10-13 15:44:48 -0500

    randomize homepage pokemon

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,19 @@ import eevee from "../samples/eevee";
 import pokemon from "../samples/pokemon";
 
 export default function Home() {
-	
+	const [randomize, setRandomize] = useState(0);
+	const [shuffle, setShuffle] = useState(false);
+
+	const shufflePokemon = (e) => {
+		e.preventDefault();
+		setShuffle(true);
+	}
+
+	useEffect(() => {
+		setRandomize(Math.floor(Math.random() * ((pokemon.results.length - 6) - 0 + 1) + 0));
+		setShuffle(false);
+	}, [shuffle]);
+		
 	return (
         <main className={styles.homepage}>
             <Head>
@@ -20,12 +32,13 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="wrapper">
-                {pokemon.results.slice(0, 6).map((character, index) => {
-                    return (
-                        <p key={index}>{character.name}</p>
-                    )
-                })}
+			<div className="wrapper">
+				{pokemon.results.slice(randomize, randomize + 6).map((character, index) => {
+					return (
+						<p key={index}>{character.name}</p>
+					)
+				})}
+				<button className={styles.homepage__btn} onClick={(e) => shufflePokemon(e)}>Shuffle Pokémon</button>
             </div>
         </main>
     );
